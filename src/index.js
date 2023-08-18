f9158cccee6d6582b6650f3d81c731b2b6c61927
Iryna Burlaka <i.am.blinova@gmail.com>
2023-08-18 19:18:48 +0200

    input style

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const { default: axios } = require('axios');
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+export axios from 'axios';
+
+// Notiflix.Notify.failure('Qui timide rogat docet negare');
+
+// Notiflix.Notify.failure('Click Me', {
+//   timeout: 3000,
+// });
